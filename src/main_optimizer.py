2f5045bba4beaa7971bbd1862d48ab76c4f84ddd
Muhammad Fithra Rizki <167663689+fithrarzk@users.noreply.github.com>
2025-11-11 22:47:42 +0700

    feat: add main for optimizer

---
+++ src/main_optimizer.py
@@ -0,0 +1,25 @@
+from .parser.parser import parse_query
+# from .optimizer.plan_optimizer import optimize_tree
+# from .optimizer.cost_estimator import estimate_cost
+
+class QueryOptimizer:
+    def parse_query(self, query: str):
+        return parse_query(query)
+
+    # def optimize_query(self, parsed_query):
+    #     pass
+
+    # def get_cost(self, parsed_query):
+    #     pass
+
+def main():
+    optimizer = QueryOptimizer()
+    parsed_query = optimizer.parse_query("SELECT p.payment_id FROM payment p WHERE p.value < 1000")
+    print("Parsed Query:")
+    print(f"Original query: {parsed_query.query}")
+    print(f"Tables: {parsed_query.tables}")
+    print("Query Tree:")
+    parsed_query.print_tree()
+
+if __name__ == "__main__":
+    main()
