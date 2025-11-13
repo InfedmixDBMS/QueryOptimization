a47c23262290c29706eaec0a2f8ce6a2c74d2a59
adndax <adindaputri0517@gmail.com>
2025-11-13 20:26:20 +0700

    fix: params for tokenize

--- src/parser/lexer.py
+++ src/parser/lexer.py
@@ -13,7 +13,7 @@ class Lexer:
     }
 
 
-    def tokenize(query: str):
+    def tokenize(self, query: str):
         """
         Tokenize a SQL query string into tokens
         """
@@ -21,4 +21,4 @@ class Lexer:
         pattern = (r"'[^']*'|\"[^\"]*\"|[A-Za-z_][A-Za-z0-9_.]*|"
                 r"<=|>=|<>|!=|=|<|>|\*|,|\(|\)|\d+")
         tokens = re.findall(pattern, query)
-        return [t.upper() if t.upper() in KEYWORDS else t for t in tokens]
+        return [t.upper() if t.upper() in self.KEYWORDS else t for t in tokens]
